9980e081b60fc37dd225178365cdc7af9cdc53ec
auto circleci <autp@example.com>
2017-08-05 23:49:26 +0900

    update version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ entry_points = {
 
 setup(
     name="miyadaiku",
-    version="0.0.19",
+    version="0.0.20",
     author="Atsuo Ishimoto",
     url='https://miyadaiku.github.io/',
     license="MIT",
