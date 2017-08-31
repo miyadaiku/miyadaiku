7c0644b60d663997e622da231b89a1e0f942b2e0
atsuo ishimoto <xatsuoi@gmail.com>
2017-09-01 00:34:02 +0900

    bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ entry_points = {
 
 setup(
     name="miyadaiku",
-    version="0.0.34",
+    version="0.0.35",
     author="Atsuo Ishimoto",
     url='https://miyadaiku.github.io/',
     license="MIT",
