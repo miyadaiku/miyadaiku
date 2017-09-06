7976babe60ad13ec1d4a3d0a718a9040e0d9faf3
atsuo ishimoto <xatsuoi@gmail.com>
2017-09-06 13:47:13 +0900

    remove profiler

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ entry_points = {
 
 setup(
     name="miyadaiku",
-    version="0.0.38",
+    version="0.0.39",
     author="Atsuo Ishimoto",
     url='https://miyadaiku.github.io/',
     license="MIT",
