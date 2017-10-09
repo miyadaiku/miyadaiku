a2e3654439489430270a7fc3ac6eb13d4b3bc139
atsuo ishimoto <xatsuoi@gmail.com>
2017-10-09 11:26:26 +0900

    add css for pygments styles

---
+++ test/test_theme_pygments.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from miyadaiku.core import rst, contents, config, jinjaenv, main
+
+
+def test_theme_pygment(sitedir):
+    sitedir.joinpath('config.yml').write_text('''
+
+themes:
+    - miyadaiku.themes.pygments
+
+''')
+
+    sitedir.joinpath('contents/index.rst').write_text('''
+title
+----------------
+.. jinja::
+
+   {{ pygments.load_css(page) }}
+''')
+
+    site = main.Site(sitedir)
+    site.build()
+
+    ret = sitedir.joinpath('outputs/index.html').read_text()
+    
+    print(ret)
+    assert '<link href="static/pygments/pygments_native.css" rel="stylesheet"/>' in ret
+    assert (sitedir / 'outputs/static/pygments/pygments_native.css').exists()
