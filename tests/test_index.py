279af6ce1c2639ceda9c814065f0209853be5a86
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-11 01:48:02 +0900

    heh

---
+++ tests/test_index.py
@@ -0,0 +1,33 @@
+from typing import cast, List
+from miyadaiku import builder
+from conftest import SiteRoot
+
+
+def test_index_group(siteroot: SiteRoot) -> None:
+    for i in range(21):
+        tag = f'tag{i % 2 + 1}'
+        siteroot.write_text(siteroot.contents / f"htmldir/{i}.html", f"""
+tags: {tag}
+
+html{i} - tag: {tag}
+""")
+
+
+    siteroot.write_text(
+        siteroot.contents / "htmldir/index.yml",
+        """
+type: index
+groupby: tags
+""",
+    )
+
+    site = siteroot.load({}, {})
+    builders = builder.create_builders(
+        site, site.files.get_content((("htmldir",), "index.yml"))
+    )
+    indexbuilders = cast(List[builder.IndexBuilder], builders)
+    for b in indexbuilders:
+        ctx = b.build_context(site)
+        f, = ctx.build()
+        print(open(f).read())
+        break
