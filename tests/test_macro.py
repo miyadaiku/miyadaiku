d53993070c36bc483f76b1bbdec5ca465585a6ce
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-04-17 10:38:48 +0900

    heh

---
+++ tests/test_macro.py
@@ -0,0 +1,69 @@
+from pathlib import Path
+from miyadaiku import rst, contents, config, jinjaenv
+from miyadaiku.site import Site
+from conftest import create_contexts, SiteRoot
+
+
+def test_ga(siteroot: SiteRoot):
+    (ctx,) = create_contexts(
+        siteroot,
+        srcs=[
+            (
+                "index.rst",
+                """
+title
+----------------
+.. jinja::
+
+   {{ macros.google_analytics() }}
+""",
+            )
+        ],
+    )
+
+
+
+    ctx.site.config.add('/', {'ga_tracking_id': '12345'})
+    output, = ctx.build()
+
+    assert "ga('create', '12345', 'auto')" in output.read_text()
+
+
+def test_image(siteroot: SiteRoot):
+
+    siteroot.write_text(siteroot.contents / 'img/img.png', '')
+    siteroot.write_text(siteroot.contents / 'index.rst', '''
+.. jinja::
+
+   {{ macros.image(page.load('/img/img.png')) }}
+''')
+
+    site = siteroot.load({}, {})
+    site.build()
+
+    ret = (siteroot.outputs / 'index.html').read_text()
+    assert 'img/img.png' in ret
+
+
+def test_og(siteroot: SiteRoot):
+    siteroot.write_text(siteroot.templates/ 'page_article.html', '''
+{{ macros.opengraph(page) }}
+''')
+
+    siteroot.write_text(siteroot.contents / 'img/img.png', '')
+    siteroot.write_text(siteroot.contents / 'index.rst', '''
+.. article::
+   :og_image: /img/img.png
+
+test article
+------------------
+
+body
+
+''')
+
+    site = siteroot.load({}, {})
+    site.build()
+
+    ret = (siteroot.outputs / 'index.html').read_text()
+    assert 'content="http://localhost:8888/img/img.png"' in ret
