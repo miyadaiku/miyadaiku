a475d1da56d22aaf77892755c5f296ed8daf522a
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2020-08-28 17:44:18 +0900

    bump version

--- miyadaiku/__init__.py
+++ miyadaiku/__init__.py
@@ -9,7 +9,7 @@ from typing import Any, Dict, List, NamedTuple, Optional, Sequence, Set, Tuple, 
 import importlib_resources
 import tzlocal
 
-__version__ = "1.18.dev1"
+__version__ = "1.18.dev2"
 
 YAML_ENCODING = "utf-8"
 
